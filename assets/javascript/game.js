8678dc02f9c825c68eb442d27e2bba47dcde6697
Mattlomet <mattlomet@live.com>
2018-06-12 20:12:44 -0400

    initial shell first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var wordReveal = document.getElementById("wordReveal");
+var letterGuessed = document.getElementById("letter Guessed");
+var guessesRemaining = document.getElementById("guessesRemaining");
+
+
